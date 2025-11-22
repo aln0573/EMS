6805b63146bea598b4894e4ac2e9042b373f1e47
unknown <alan8589824041@gmail.com>
2025-11-22 15:46:47 +0530

    login controller added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt  from 'bcrypt'
 import User from '../models/User.js'
+import jwt from 'jsonwebtoken'
 
 export const Register = async (req, res) => {
     try {
@@ -12,6 +13,11 @@ export const Register = async (req, res) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         if(!emailRegex.test(email)){
             return res.status(400).json({message: "Invalid email format"})
+        }
+
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+            return res.status(400).json({message: "User already exists"})
         }
 
         if(password.length < 6) {
@@ -37,5 +43,40 @@ export const Register = async (req, res) => {
     } catch (error) {
         console.error('Error in Register Controler', error.message);
         res.status(500).json({message: "Internal server error"})
+    }
+}
+
+
+
+export const Login = async (req ,res) => {
+    try {
+        const { email , password } = req.body
+        
+        if(!email || !password) {
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message: "User not found"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+
+        res.status(200).json({message: "Login Successfull", token, user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        }})
+    } catch (error) {
+        console.error("Error in Login Controller:", error.message)
+        res.status(500).json({ message: "Internal server error" })
     }
 }

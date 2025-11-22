5bfb97dc3c351ea74fa9d16ab4ce2497eb05d40a
unknown <alan8589824041@gmail.com>
2025-11-22 19:42:48 +0530

    employee updated

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { protectedRoute } from '../middlewares/auth.middleware.js'
 import { adminOnly } from '../middlewares/admin.middleware.js'
-import { addEmployee,} from '../controllers/admin.controller.js'
+import { addEmployee, editEmployee,  } from '../controllers/admin.controller.js'
 import upload from '../middlewares/multer.js'
 
 const router = express.Router()
 
 router.post("/employees", protectedRoute, adminOnly, upload.single("profilePic"), addEmployee)
+router.put("/employees/:id", protectedRoute, adminOnly, upload.single("profilePic"), editEmployee)
 
 
 export default router

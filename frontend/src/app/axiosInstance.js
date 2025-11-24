92ca2c41a04cfd0590275f3089da6a2ee609ac2e
unknown <alan8589824041@gmail.com>
2025-11-24 11:03:58 +0530

    added secure urls

--- frontend/src/app/axiosInstance.js
+++ frontend/src/app/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosIntance = axios.create({
-    baseURL: "http://localhost:4001/api",
+    baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true,
 })
 
